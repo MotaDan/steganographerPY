14513ba25cbf18aa11981190e62f557806de5217
Alexis Blanco <Alexis Blanco>
2016-08-28 17:44:20 -0400

    Updated python version to 3.5 and fixed any code problems that came from that update.

--- steganographer.py
+++ steganographer.py
@@ -12,6 +12,7 @@ def hideByte(cleanData, val):
 	
 	return hiddenData
 
+
 # Expects a bytearray of length 8. Will pull out the least significant bit from each byte and return them.
 def revealByte(hiddenData):
 	revealedData = bytearray(1)
@@ -22,16 +23,19 @@ def revealByte(hiddenData):
 	
 	return revealedData
 
+
 # Expects a bytearray of any length and a string value. Will return a bytearray with the string's bits 
 # hidden in the least significant bits.
 def hideString(cleanData, val):
-	return hideData(cleanData, bytearray(val))
+	return hideData(cleanData, bytearray(val, 'utf-8'))
+
 
 # Expects a bytearray of any length. Will pull out the least significant bits from each byte and 
 # return them as a string.
 def revealString(hiddenData):
-	return str(revealData(hiddenData))
-	
+	return revealData(hiddenData).decode()
+
+
 # Expects a bytearray cleanData of any length and another bytearray val. Will return a bytearray with the val's bits 
 # hidden in the least significant bits of cleanData.
 def hideData(cleanData, val):
@@ -43,10 +47,11 @@ def hideData(cleanData, val):
 	
 	return hiddenData
 
+
 # Expects a bytearray hiddenData of any length. Will pull out the least significant bits from each byte and 
 # return them as a byteArray.
 def revealData(hiddenData):
-	revealedDataLen = len(hiddenData) / byteLen
+	revealedDataLen = int(len(hiddenData) / byteLen)
 	revealedData = bytearray()
 	
 	for i in range(0, revealedDataLen * byteLen, byteLen):
@@ -59,6 +64,7 @@ def revealData(hiddenData):
 	
 	return revealedData
 
+
 # Testing class
 import unittest
 import time
@@ -67,103 +73,129 @@ class TestSteganographer(unittest.TestCase):
 	def setUp(self):
 		self.startTime = time.time()
 	
+	
 	def tearDown(self):
 		t = time.time() - self.startTime
-		print "Ran in %.3fs " % (t),
+		print("Ran in %.3fs " % (t))
 	
+	
+	# Testing that the hideByte function does hide a byte and returns the testData with that byte hidden.
 	def test_hideByte(self):
 		testData = bytearray(byteLen)
-		dataToHide = bytearray('A')
+		dataToHide = bytearray('A', 'utf-8')
 		solutionData = bytearray(byteLen)
-		solutionData[1] = chr(1)
-		solutionData[7] = chr(1)
-		self.assertEqual(hideByte(testData, dataToHide[0]), solutionData)
+		solutionData[1] = 1
+		solutionData[7] = 1
 		
+		self.assertEqual(hideByte(testData, dataToHide[0]), solutionData)
+	
+	
+	# Testing that the revealByte function returns a bytearray of the hidden byte. 
 	def test_revealByte(self):
 		testData = bytearray(byteLen)
-		testData[1] = chr(1)
-		testData[7] = chr(1)
-		solutionData = bytearray('A')
+		testData[1] = 1
+		testData[7] = 1
+		solutionData = bytearray('A', 'utf-8')
+		
 		self.assertEqual(revealByte(testData), solutionData)
 	
+	
+	# Testing that hideString takes in a string and bytearray and hides the string in that bytearray.
 	def test_hideString(self):
 		testData = bytearray(byteLen * 3)
 		solutionData = bytearray(byteLen * 3)
-		solutionData[1] = chr(1)
-		solutionData[7] = chr(1)
-		solutionData[9] = chr(1)
-		solutionData[14] = chr(1)
-		solutionData[17] = chr(1)
-		solutionData[22] = chr(1)
-		solutionData[23] = chr(1)
-		self.assertEqual(hideString(testData, 'ABC'), solutionData)
+		solutionData[1] = 1
+		solutionData[7] = 1
+		solutionData[9] = 1
+		solutionData[14] = 1
+		solutionData[17] = 1
+		solutionData[22] = 1
+		solutionData[23] = 1
 		
+		self.assertEqual(hideString(testData, 'ABC'), solutionData)
+	
+	
+	# Testing that revealString returns a string of the data that was hidden in testData.
 	def test_revealString(self):
 		testData = bytearray(byteLen * 3)
-		testData[1] = chr(1)
-		testData[7] = chr(1)
-		testData[9] = chr(1)
-		testData[14] = chr(1)
-		testData[17] = chr(1)
-		testData[22] = chr(1)
-		testData[23] = chr(1)
+		testData[1] = 1
+		testData[7] = 1
+		testData[9] = 1
+		testData[14] = 1
+		testData[17] = 1
+		testData[22] = 1
+		testData[23] = 1
+		
 		self.assertEqual(revealString(testData), 'ABC')
 	
+	
+	# Testing that hideData will hide one bytearray inside another.
 	def test_hideData(self):
 		testData = bytearray(byteLen * 3)
-		dataToHide = bytearray('ABC')
+		dataToHide = bytearray('ABC', 'utf-8')
 		solutionData = bytearray(byteLen * 3)
-		solutionData[1] = chr(1)
-		solutionData[7] = chr(1)
-		solutionData[9] = chr(1)
-		solutionData[14] = chr(1)
-		solutionData[17] = chr(1)
-		solutionData[22] = chr(1)
-		solutionData[23] = chr(1)
-		self.assertEqual(hideData(testData, dataToHide), solutionData)
+		solutionData[1] = 1
+		solutionData[7] = 1
+		solutionData[9] = 1
+		solutionData[14] = 1
+		solutionData[17] = 1
+		solutionData[22] = 1
+		solutionData[23] = 1
 		
+		self.assertEqual(hideData(testData, dataToHide), solutionData)
+	
+	
+	# Testing that hideData will work correctly when given a testData bytearray that is too short to contain the data given.
 	def test_hideDataPartial(self):
 		testData = bytearray(byteLen * 3)
-		dataToHide = bytearray('ABC')
+		dataToHide = bytearray('ABC', 'utf-8')
 		solutionData = bytearray(byteLen * 3)
-		solutionData[1] = chr(1)
-		solutionData[7] = chr(1)
-		solutionData[9] = chr(1)
-		solutionData[14] = chr(1)
-		solutionData[17] = chr(1)
-		solutionData[22] = chr(1)
-		solutionData[23] = chr(1)
+		solutionData[1] = 1
+		solutionData[7] = 1
+		solutionData[9] = 1
+		solutionData[14] = 1
+		solutionData[17] = 1
+		solutionData[22] = 1
+		solutionData[23] = 1
+		
 		# Testing when only half a byte is passed in for the data that contains the hidden text.
 		self.assertEqual(hideData(testData[:4], dataToHide), solutionData[:4])
 	
+	
+	# Testing that revealData will return the correct data that is hidden inside the testData.
 	def test_revealData(self):
-		solutionData = bytearray('ABC')
 		testData = bytearray(byteLen * 3)
-		testData[1] = chr(1)
-		testData[7] = chr(1)
-		testData[9] = chr(1)
-		testData[14] = chr(1)
-		testData[17] = chr(1)
-		testData[22] = chr(1)
-		testData[23] = chr(1)
-		self.assertEqual(revealData(testData), solutionData)
+		testData[1] = 1
+		testData[7] = 1
+		testData[9] = 1
+		testData[14] = 1
+		testData[17] = 1
+		testData[22] = 1
+		testData[23] = 1
+		solutionData = bytearray('ABC', 'utf-8')
 		
+		self.assertEqual(revealData(testData), solutionData)
+	
+	
+	# Testing that reveal data will return as much data as possible when the testData passed in is too small for the data to be hidden.
 	def test_revealDataPartial(self):
-		solutionData = bytearray('AB@')
 		testData = bytearray(byteLen * 3)	#Will contain 'ABC' but will be truncated when passed to revealData
-		testData[1] = chr(1)
-		testData[7] = chr(1)
-		testData[9] = chr(1)
-		testData[14] = chr(1)
-		testData[17] = chr(1)
-		testData[22] = chr(1)
-		testData[23] = chr(1)
-		self.assertEqual(revealData(testData[:len(testData) - byteLen/2]), solutionData)
-
+		testData[1] = 1
+		testData[7] = 1
+		testData[9] = 1
+		testData[14] = 1
+		testData[17] = 1
+		testData[22] = 1
+		testData[23] = 1
+		solutionData = bytearray('AB@', 'utf-8')
+		
+		self.assertEqual(revealData(testData[:-int(byteLen/2)]), solutionData)
+	
+	
 	# Testing that the string entered is the string returned. The data it is stored in is the exact length needed.
 	def test_steganographerNullData(self):
 		testString = "This is a test String"
-		testData = bytearray(testString)
+		testData = bytearray(testString, 'utf-8')
 		blankData = bytearray(len(testString) * byteLen)
 		
 		hiddenString = hideString(blankData, testString)
@@ -174,11 +206,12 @@ class TestSteganographer(unittest.TestCase):
 		
 		self.assertEqual(testString, revealedString)
 		self.assertEqual(testData, revealedData)
-		
-	# Testing that when the data is too small, by a full byte, that everything that can be returned is.
+	
+	
+	# Testing that when the data is too small, by a full byte, that everything that can be returned is returned.
 	def test_steganographerShortData(self):
 		testString = "This is a test String"
-		testData = bytearray(testString)
+		testData = bytearray(testString, 'utf-8')
 		blankData = bytearray(len(testString) * byteLen - byteLen)
 		
 		hiddenString = hideString(blankData, testString)
@@ -187,14 +220,18 @@ class TestSteganographer(unittest.TestCase):
 		hiddenData = hideData(blankData, testData)
 		revealedData = revealData(hiddenData)
 		
-		self.assertEqual(testString[:len(testString) - 1], revealedString)
-		self.assertEqual(testData[:len(testData) - 1], revealedData)
-		
-	# Testing that when the data is too small, by a half byte, that everything that can be returned is.
+		self.assertEqual(testString[:-1], revealedString)
+		self.assertEqual(testData[:-1], revealedData)
+	
+	
+	# Testing that when the data is too small, by a half byte, that everything that can be returned is returned.
 	def test_steganographerShortPartialData(self):
 		testString = "This is a test String"
-		testData = bytearray(testString)
-		blankData = bytearray(len(testString) * byteLen - byteLen / 2)
+		solutionString = testString[:-1] + chr(ord(testString[-1]) >> int(byteLen/2) << int(byteLen/2))
+		testData = bytearray(testString, 'utf-8')
+		solutionData = testData
+		solutionData[-1] = solutionData[-1] >> int(byteLen/2) << int(byteLen/2)
+		blankData = bytearray(len(testString) * byteLen - int(byteLen / 2))
 		
 		hiddenString = hideString(blankData, testString)
 		revealedString = revealString(hiddenString)
@@ -202,10 +239,11 @@ class TestSteganographer(unittest.TestCase):
 		hiddenData = hideData(blankData, testData)
 		revealedData = revealData(hiddenData)
 		
-		self.assertEqual(testString[:len(testString) - 1] + chr(ord(testString[-1]) >> (byteLen/2) << (byteLen/2)), revealedString)
-		self.assertEqual(testData[:len(testData) - 1] + chr(testData[-1] >> (byteLen/2) << (byteLen/2)), revealedData)
+		self.assertEqual(solutionString, revealedString)
+		self.assertEqual(solutionData, revealedData)
+	
 	
 if __name__ == '__main__':
-	print "Preparing tests..."
+	print("Preparing tests...")
 	runner = unittest.TextTestRunner(verbosity = 2)
 	unittest.main(testRunner = runner)
