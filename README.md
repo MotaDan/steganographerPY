400076622ec9584d24f5f0abe1ff749e065c4f28
Daniel Mota <AndroidKing@gmail.com>
2016-09-10 00:14:23 -0400

    Taking parts from a pyscaffold setup and applying them here.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# steganographerPY
+==============
+steganographer
+==============
 [![Build Status](https://travis-ci.org/MotaDan/steganographerPY.svg?branch=master)](https://travis-ci.org/MotaDan/steganographerPY)
 [![Coverage Status](https://coveralls.io/repos/github/MotaDan/steganographerPY/badge.svg?branch=master)](https://coveralls.io/github/MotaDan/steganographerPY?branch=master)
 [![Code Health](https://landscape.io/github/MotaDan/steganographerPY/master/landscape.svg?style=flat)](https://landscape.io/github/MotaDan/steganographerPY/master)
+
+
+Add a short description here!
+
+
+Description
+===========
+
+A LONGER DESCRIPTION OF YOUR PROJECT GOES HERE...
+
+
+Note
+====
+
+This project has been set up using PyScaffold 2.5.6. For details and usage
+information on PyScaffold see http://pyscaffold.readthedocs.org/.
--- setup.py
+++ setup.py
@@ -1,100 +1,23 @@
-"""A setuptools based setup module.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Setup file for steganographer.
 
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+    This file was generated with PyScaffold 2.5.6, a tool that easily
+    puts up a scaffold for your new Python project. Learn more under:
+    http://pyscaffold.readthedocs.org/
 """
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+import sys
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+def setup_package():
+    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
+    sphinx = ['sphinx'] if needs_sphinx else []
+    setup(setup_requires=['six', 'pyscaffold>=2.5a0,<2.6a0'] + sphinx,
+          use_pyscaffold=True)
 
-setup(
-    name='steganographer',
 
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0',
-
-    description='Hides messages inside images',
-    long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/MotaDan/steganographerPY',
-
-    # Author details
-    author='Daniel Mota',
-    author_email='AndroidKing+steganographer@gmail.com',
-
-    # Choose your license
-    license='MIT',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.5-dev',
-		'Programming Language :: Python :: nightly',
-    ],
-
-    # What does your project relate to?
-    keywords='steganography hidden',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['Pillow'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'steganographer=steganographer:main',
-        ],
-    },
-)
+if __name__ == "__main__":
+    setup_package()
