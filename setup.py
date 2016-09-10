e820b105d00680a81fedee7da84b18ccd0b96ffa
Daniel Mota <AndroidKing@gmail.com>
2016-09-09 20:58:11 -0400

    Adding more recent versions of python to setup.

--- setup.py
+++ setup.py
@@ -59,6 +59,8 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.5-dev',
+		'Programming Language :: Python :: nightly',
     ],
 
     # What does your project relate to?
