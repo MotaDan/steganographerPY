873b43772319253f1cc15d0749ebc30624357bf8
Daniel Mota <AndroidKing@gmail.com>
2016-09-10 00:51:32 -0400

    Got the import in test_steganographer working again. The tests are now roken because they don't see the iamge files.

--- tests/test_steganographer.py
+++ tests/test_steganographer.py
@@ -1,9 +1,12 @@
 # Testing class
 import unittest
 import time
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from steganographer.steganographer import *
 import os
-import os.path
+#import os.path
 
 class TestSteganographer(unittest.TestCase):
 	def setUp(self):
