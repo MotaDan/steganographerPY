4a6b7fe54b792d2e4b294abde175b6253d2d7d45
Daniel Mota <AndroidKing@gmail.com>
2016-09-14 17:22:30 -0400

    Changing readme from markdown to reStructuredText.

---
+++ README.rst
@@ -0,0 +1,19 @@
+==============
+steganographer
+==============
+.. image:: https://travis-ci.org/MotaDan/steganographerPY.svg?branch=master
+   :target: https://travis-ci.org/MotaDan/steganographerPY
+.. image:: https://coveralls.io/repos/github/MotaDan/steganographerPY/badge.svg?branch=master
+   :target: https://coveralls.io/github/MotaDan/steganographerPY?branch=master
+.. image:: https://landscape.io/github/MotaDan/steganographerPY/master/landscape.svg?style=flat
+   :target: https://landscape.io/github/MotaDan/steganographerPY/master
+   :alt: Code Health
+
+
+Hide messages inside an image. 
+
+
+Description
+===========
+
+Given an image and a message steganographer will hide the message in the bits of the image. Currently only works for pngs.
--- setup.py
+++ setup.py
@@ -14,11 +14,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except ImportError:
-    long_description = open('README.md').read()
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
 
 setup(
 	name='steganographer',
@@ -26,7 +23,7 @@ setup(
 	# Versions should comply with PEP440.  For a discussion on single-sourcing
 	# the version across setup.py and the project code, see
 	# https://packaging.python.org/en/latest/single_source_version.html
-	version='1.0.1',
+	version='1.0.0',
 
 	description='Hides a message inside an image.',
 	long_description=long_description,
