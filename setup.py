4d3e0ea548f011c85cfb9e58836719f024125d3b
Daniel Mota <AndroidKing@gmail.com>
2016-09-21 17:39:38 -0400

    #45: The long description in setup can not have Windows line ending. Removing python versions that are not supported by setup.

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 	long_description = f.read()
+	long_description = long_description.replace("\r","")
 
 setup(
 	name='steganographer',
@@ -63,8 +64,6 @@ setup(
 		'Programming Language :: Python :: 3.3',
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.5-dev',
-		'Programming Language :: Python :: nightly',
 		'Environment :: Console',
 		'Environment :: Win32 (MS Windows)',
 		
