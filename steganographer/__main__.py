d8224e0825f68b0289cf46bdc99d313d29405300
Daniel Mota <AndroidKing@gmail.com>
2017-12-27 19:22:05 -0500

    Issue #82: Removed the output file paths in main along with the if check for an input file. Input is a default and argparse checks that already.

--- steganographer/__main__.py
+++ steganographer/__main__.py
@@ -10,7 +10,7 @@ def main():
     parser.add_argument("input", help="file to hide a message in or file to reveal a message from")
     parser.add_argument("-m", "--message", help="message to be hidden in the input file")
     parser.add_argument("-o", "--output",
-                        help="name of output file to hide message in or to write revealed message")
+                        help="name of output file to hide message in or to write revealed message", default='')
     parser.add_argument("-f", "--file", help="file to be hidden in the input file")
     parser.add_argument("-r", "--reveal", action='store_true', help="a file will be revealed")
     parser.add_argument("-v", "--version", action='version', version="steganographer {}".format(pkg_resources.get_distribution('steganographer').version), help="show version and exit")
@@ -18,52 +18,46 @@ def main():
 
     stegs = Steganographer()
 
-    if args.input:
-        # There is a message to hide.
-        if args.message:
-            if args.output:
-                hidden_fname = stegs.steganographer_hide(args.input, args.message, args.output)
-            else:
-                hidden_fname = stegs.steganographer_hide(args.input, args.message)
-            print("The message has been hidden in " + hidden_fname)
-        # There is a file to hide.
-        elif args.file:
-            if args.output:
-                hidden_fname = stegs.steganographer_hide_file(args.input, args.file, args.output)
-            else:
-                hidden_fname = stegs.steganographer_hide_file(args.input, args.file)
-            print("The file " + args.file + " has been hidden in " + hidden_fname)
-        # Revealing a file.
-        elif args.reveal:
-            revealed_data = stegs.steganographer_reveal(args.input)
+    # There is a message to hide.
+    if args.message:
+        print("output provided")
+        hidden_fname = stegs.steganographer_hide(args.input, args.message, args.output)
+        print("The message has been hidden in " + hidden_fname)
+    # There is a file to hide.
+    elif args.file:
+        hidden_fname = stegs.steganographer_hide_file(args.input, args.file, args.output)
+        print("The file " + args.file + " has been hidden in " + hidden_fname)
+    # Revealing a file.
+    elif args.reveal:
+        revealed_data = stegs.steganographer_reveal(args.input)
 
-            if args.output:
-                with open(args.output, 'wb') as rev_file:
-                    rev_file.write(revealed_data)
-                print("The hidden file was revealed in " + args.output)
-            else:
-                file_name = stegs._header.file_name.decode('utf-8')
-
-                with open(file_name, 'wb') as rev_file:
-                    rev_file.write(revealed_data)
-                print("The hidden file was revealed in " + file_name)
-        # Revealing a message.
+        if args.output:
+            with open(args.output, 'wb') as rev_file:
+                rev_file.write(revealed_data)
+            print("The hidden file was revealed in " + args.output)
         else:
-            hidden_message = stegs.steganographer_reveal(args.input).decode('utf-8')
+            file_name = stegs._header.file_name.decode('utf-8')
 
-            if args.output:
-                open(args.output, 'w', encoding='utf-8').write(hidden_message)
-                print("The hidden message was written to " + args.output)
-            else:
-                try:
-                    print("The hidden message was...\n" + hidden_message)
-                except UnicodeEncodeError:  # pragma: no cover
-                    output_name = args.input.split('.')[0] + '_message.txt'
+            with open(file_name, 'wb') as rev_file:
+                rev_file.write(revealed_data)
+            print("The hidden file was revealed in " + file_name)
+    # Revealing a message.
+    else:
+        hidden_message = stegs.steganographer_reveal(args.input).decode('utf-8')
 
-                    print("The hidden message contains unsupported unicode characters and cannot be fully displayed " +
-                          "here. The correct message has been written to", output_name)
-                    print(hidden_message.encode('utf-8'))
-                    open(output_name, 'w', encoding='utf-8').write(hidden_message)
+        if args.output:
+            open(args.output, 'w', encoding='utf-8').write(hidden_message)
+            print("The hidden message was written to " + args.output)
+        else:
+            try:
+                print("The hidden message was...\n" + hidden_message)
+            except UnicodeEncodeError:  # pragma: no cover
+                output_name = args.input.split('.')[0] + '_message.txt'
+
+                print("The hidden message contains unsupported unicode characters and cannot be fully displayed " +
+                      "here. The correct message has been written to", output_name)
+                print(hidden_message.encode('utf-8'))
+                open(output_name, 'w', encoding='utf-8').write(hidden_message)
 
 
 if __name__ == "__main__":
